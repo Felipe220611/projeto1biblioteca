15ac3d9c1770d53b88146a4af98375adc5627db3
Felipe <rodrigues.soares.felipe@escola.pr.gov.br>
2026-09-23 11:31:23 -0300

    Editar alunos livros e bibliotecario

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /tmp/guest-iksc7q/felipe1b/projeto1biblioteca/venv
+set -gx VIRTUAL_ENV /tmp/guest-ihuwud/felipe1b/projeto1biblioteca/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
